50427adf4d5b9b58c00af0dfeafcbef2405d28df
Giannoulakis <giannoulakis@gmail.com>
2021-01-09 13:47:47 +0700

    refactoring and optimizing functions

--- frontend/src/views/Note.vue
+++ frontend/src/views/Note.vue
@@ -6,6 +6,7 @@
 				<h1>{{ note.name }}</h1>
 			</div>
 			<p>selectedIndex:{{selectedIndex}}</p>
+
 			<div class="blocks">
 				<ul>
 					<li v-for="(block,index) in note.blocks" :key="block.id" :style="styleBlock(block.level)">
@@ -13,10 +14,10 @@
 							v-if="selectedIndex === index"
 							v-model="block.text"
 							ref="textarea"
-							@input="setInputHeight()"
-							@keydown="onKeydown($event)"
+							@input="setInputHeight"
+							@keydown="onKeydown"
 						/>
-						<p @click="selectBlock(index,$event)">{{ block.text }}</p>
+						<p @click="onClickBlock(index,$event)" v-html="marked(block.text)"></p>
 					</li>
 				</ul>
 			</div>
@@ -46,102 +47,113 @@ export default {
 		return {
 			note:{},
 			selectedIndex: -1,
-			keyUpFunctions: {
-				ArrowUp : (event) => {
-					this.keyDownFunctions.ArrowUpDown(event, -1);
-				},
-				ArrowDown: (event) => {
-					if(this.$refs.textarea.selectionStart == this.currentBlock().text.length){
-						this.keyDownFunctions.ArrowUpDown(event, +1);
-					}
-
-				},
-				ArrowUpDown: (event, move) => {
-					event.preventDefault();
-					if(event.shiftKey && event.altKey){
-						let removed = this.note.blocks.splice(this.selectedIndex,1);
-						this.note.blocks.splice(this.selectedIndex + move,0,removed[0]);
-					}
-					var cursorPosition = this.$refs.textarea.selectionStart;
-					this.selectedIndex += move;
-					this.focusTextarea(cursorPosition);
-				},
-			},
 			keyDownFunctions: {
+
 				ArrowUp : (event) => {
-					this.keyDownFunctions.ArrowUpDown(event, -1);
+					if(!this.previusBlock) return;
+					if(!this.isFirstLine()) return;
+					event.preventDefault();
+					this.selectedIndex--;
+					let relativeCursorPosition = this.cursorPosition() + this.cursorOffset();
+					this.focusTextarea(relativeCursorPosition);
 				},
 				ArrowDown: (event) => {
-					if(this.$refs.textarea.selectionStart == this.currentBlock().text.length){
-						this.keyDownFunctions.ArrowUpDown(event, +1);
-					}
+					if(!this.nextBlock) return;
+					if(!this.isLastLine()) return;
+					let relativeCursorPosition = this.cursorPosition() - this.cursorOffset();
+					//caso o proximo bloco tenha +1 linha e o cursorposition do current block é maior que o tamanho da primeira linha, define o tamanho maximo como o length da primeira linha para evitar que o cursor seja posicionado na segunda linha
+					relativeCursorPosition = Math.min(relativeCursorPosition,this.nextBlock.text.split("\n")[0].length);
 
+					setTimeout(()=>{ //com o setTimeout a gente vai saber se o cursor pulou pro final da linha
+						if(this.cursorPosition() == this.currentBlock.text.length) {
+							this.selectedIndex++;
+							this.focusTextarea(relativeCursorPosition);
+						}
+					},0);
 				},
+				ShiftAltArrowUp : (event) => {
+					if(!this.previusBlock) return;
+					event.preventDefault();
+					this.insertBlock(this.selectedIndex-1,this.removeBlock());
+					this.selectedIndex--;
+					this.focusTextarea(this.cursorPosition());
+				},
+				ShiftAltArrowDown: (event) => {
+					if(!this.nextBlock) return;
+					event.preventDefault();
+					this.insertBlock(this.selectedIndex+1,this.removeBlock());
+					this.selectedIndex++;
+					this.focusTextarea(this.cursorPosition());
+				},
+
 				Tab: (event) => {
 					event.preventDefault();
-					let move = +1;
-					if(event.shiftKey) move = -1;
-					this.note.blocks[this.selectedIndex].level += move;
-					if(this.note.blocks[this.selectedIndex].level < 0) {
-						this.note.blocks[this.selectedIndex].level = 0;
-					}
+					if(!this.previusBlock) return;
+					this.currentBlock.level = Math.min(this.previusBlock.level+1,this.currentBlock.level+1);
+				},
+				ShiftTab: (event) => {
+					event.preventDefault();
+					if(!this.previusBlock) return;
+					this.currentBlock.level	= Math.max(this.currentBlock.level-1,0);
 				},
 				Delete: (event) => {
-					let textarea = this.$refs.textarea;
-					if(textarea.selectionStart == textarea.value.length) {
-						let nextBlock = this.nextBlock();
-						if(!nextBlock) return;
-
-						event.preventDefault();
-						const size = this.currentBlock().text.length;
-						this.currentBlock().text += nextBlock.text;
-						this.removeBlock(+1);
-						this.focusTextarea(size);
-					}
-
+					if(this.cursorPosition() !== this.currentBlock.text.length) return;
+					if(!this.nextBlock) return;
+					event.preventDefault();
+					const size = this.currentBlock.text.length;
+					this.currentBlock.text += this.nextBlock.text;
+					this.removeBlock(+1);
+					this.focusTextarea(size);
+				},
+				ShiftDelete: (event) => {
+					event.preventDefault();
+					this.removeBlock();
+					this.focusTextarea(0);
 				},
 				Backspace: (event) => {
-					let textarea = this.$refs.textarea;
-					if(textarea.selectionEnd == 0) {
-						let previusBlock = this.previusBlock();
-						if(!previusBlock) return;
-
-						event.preventDefault();
-						const size = previusBlock.text.length;
-						previusBlock.text += this.currentBlock().text;
-						this.removeBlock();
-						this.selectedIndex--;
-						this.focusTextarea(size);
-					}
+					if(this.cursorPosition() !== 0) return;
+					if(!this.previusBlock) return;
+					event.preventDefault();
+					const size = this.previusBlock.text.length;
+					this.previusBlock.text += this.currentBlock.text;
+					this.removeBlock();
+					this.selectedIndex--;
+					this.focusTextarea(size);
 				},
 				Enter: (event) => {
-					if(event.shiftKey) return;
 					event.preventDefault();
 					// if(hasMenu())
-					if(event.ctrlKey) {
-						this.setTodo();
-					} else {
-						let start = this.$refs.textarea.selectionStart;
-						let textStart = this.currentBlock().text.substr(0, start);
-						let textEnd = this.currentBlock().text.substr(start);
-						this.currentBlock().text = textStart;
-						let newBlock = {
-							text: textEnd,
-							parent_id: this.currentBlock().parent_id,
-							level: this.currentBlock().level
-						};
-						this.note.blocks.splice( this.selectedIndex+1, 0, newBlock );
-						this.selectedIndex++;
-						this.focusTextarea(0);
-					}
-
-				}
+					let start = this.cursorPosition();
+					let textStart = this.currentBlock.text.substr(0, start);
+					let textEnd = this.currentBlock.text.substr(start);
+					this.currentBlock.text = textStart;
+					let newBlock = {
+						text: textEnd,
+						parent_id: this.currentBlock.parent_id,
+						level: this.currentBlock.level
+					};
+					this.insertBlock( this.selectedIndex+1, newBlock );
+					this.selectedIndex++;
+					this.focusTextarea(0);
+				},
+				CtrlEnter: (event) => {
+					this.setTodo();
+				},
 			}
 		}
 	},
 	computed: {
 		notes() {
 			return this.$store.getters.notes;
+		},
+		currentBlock() {
+			return this.note.blocks[this.selectedIndex];
+		},
+		nextBlock() {
+			return this.note.blocks[this.selectedIndex + 1];
+		},
+		previusBlock() {
+			return this.note.blocks[this.selectedIndex - 1];
 		},
 		references(){
 			if(!this.notes || !this.note.references) return false;
@@ -155,44 +167,61 @@ export default {
 		},
 	},
 	methods: {
+
+		cursorPosition(newval){
+			if(!this.$refs.textarea) return 0;
+			if(newval===undefined){
+				return this.$refs.textarea.selectionStart;
+			}
+			this.$refs.textarea.selectionStart = newval;
+			this.$refs.textarea.selectionEnd = newval;
+		},
+		cursorOffset(){
+			let lines = this.currentBlock.text.split("\n");
+			lines.pop();
+			return lines.join("\n").length
+			//let match = text.match(/^([\s\S]+)\n/); return match?match.pop().length:0;
+		},
+		isFirstLine(){
+			let lines = this.currentBlock.text.split("\n");
+			return lines[0].length >= this.cursorPosition();
+		},
+		isLastLine(){
+			let lines = this.currentBlock.text.split("\n");
+			lines.pop();
+			return lines.join("\n").length <= this.cursorPosition();
+		},
 		styleBlock(level) {
 			return 	{
 				marginLeft: `${20*level}px`
 			};
 		},
 		onKeydown(event){
-			if(typeof this.keyDownFunctions[event.key] === 'function'){
-				console.log(`Key down: ${event.key}\n---`);
-				this.keyDownFunctions[event.key](event);
+			let command = '';
+			if(event.ctrlKey) command += 'Ctrl';
+			if(event.shiftKey) command += 'Shift';
+			if(event.altKey) command += 'Alt';
+			command += event.key;
+
+			if(typeof this.keyDownFunctions[command] === 'function'){
+				console.log(`Key down: ${command}\n---`);
+				this.keyDownFunctions[command](event);
 			}
-		},
-		onKeyup(event){
-			if(typeof this.keyUpnFunctions[event.key] === 'function'){
-				console.log(`Key up: ${event.key}\n---`);
-				this.keyUpFunctions[event.key](event);
-			}
-		},
-		currentBlock() {
-			return this.note.blocks[this.selectedIndex];
-		},
-		nextBlock() {
-			return this.note.blocks[this.selectedIndex + 1];
-		},
-		previusBlock() {
-			return this.note.blocks[this.selectedIndex - 1];
 		},
 		removeBlock(nearBy) {
 			if(!nearBy) nearBy = 0;
 			let removed = this.note.blocks.splice(this.selectedIndex + nearBy,1);
 			return removed[0];
 		},
+		insertBlock(index,block) {
+			this.note.blocks.splice(index,0,block);
+		},
 		focusTextarea(cursorPosition){
 			console.log('cursorPosition',cursorPosition);
 			this.$nextTick(function(){
 				console.log('textarea depois',this.$refs.textarea);
 				this.$refs.textarea.focus();
-				this.$refs.textarea.selectionStart = cursorPosition;
-				this.$refs.textarea.selectionEnd = cursorPosition;
+				this.cursorPosition(cursorPosition)
 				this.setInputHeight();
 			});
 		},
@@ -205,10 +234,17 @@ export default {
 
 			var cursorPosition = window.getSelection().getRangeAt(0).endOffset;
 			this.focusTextarea(cursorPosition);
-
+		},
+		onClickBlock(index,event){
+			if(event.target.matches('.checkbox')){
+				let block = this.note.blocks[index];
+				this.toggleCheckbox(block)
+				return;
+			}
+			this.selectBlock(index,event);
 		},
 		setTodo() {
-			let text = this.currentBlock().text;
+			let text = this.currentBlock.text;
 
 			if (!text.match(/^\[\[(TODO|DONE)\]\]/)) {
 				text = "[[TODO]] " + text;
@@ -217,7 +253,40 @@ export default {
 			} else if (text.match(/^\[\[DONE\]\]/)) {
 				text = text.replace(/^\[\[DONE\]\] */, "");
 			}
-			this.currentBlock().text = text;
+			this.currentBlock.text = text;
+		},
+		toggleCheckbox(block) {
+			let text = block.text;
+			if (text.match(/^\[\[TODO\]\]/)) {
+				text = text.replace(/^\[\[TODO\]\]/, "[[DONE]]");
+			} else if (text.match(/^\[\[DONE\]\]/)) {
+				text = text.replace(/^\[\[DONE\]\]/, "[[TODO]]");
+			}
+			block.text = text;
+		},
+		marked(text){
+			if(!text) return '';
+			text = text.replace(/\[\[TODO\]\]/g,'<span class="checkbox"></span>')
+			text = text.replace(/\[\[DONE\]\]/g,'<span class="checkbox checked"></span>')
+
+			text = text.replace(/\*\*([^\*]+)\*\*/g,'<strong>$1</strong>')
+			text = text.replace(/__([^_]+)__/g,'<strong>$1</strong>')
+			text = text.replace(/~~([^~]+)~~/g,'<del>$1</del>')
+
+			text = text.replace(/\*([^\*]+)\*/g,'<em>$1</em>')
+			text = text.replace(/_([^_]+)_/g,'<em>$1</em>')
+/*
+			var matches = text.match(/\[\[([^\]]+)\]\]/g);
+			if(matches) matches.forEach(match=>{
+				var note_name = match.replace(/[\[\]]/g,'');
+				var note = store.state.notes.find(note=>{
+					return note.name == note_name
+				});
+				if(!note) return;
+				text = text.replace(match,`<a class="reference" href="/${note.slug}">${match}</a>`);
+			});
+*/
+			return text;
 		}
 	},
 	mounted() {
